b7d2e51f3adccabf4f6b6ad06c57c269e5de05d9
DaenenTDD <44416985+DaenenTDD@users.noreply.github.com>
2023-08-26 05:31:13 -0600

    Player class cleaned up (TODO: backwards blue man)

--- main.py
+++ main.py
@@ -31,166 +31,9 @@ shoot2 = False
 knife = False
 knife2 = False
 tile_size = 25
-class Player2():
-    def __init__(self, x, y):
-        self.images_right = []
-        self.images_left = []
-        self.images_right_openchest = []
-        self.images_left_openchest = []
-        self.index = 0
-        self.counter = 0
-        self.grounded = False
-        self.direction = 0
-        self.shoot2_cooldown = 0
-        self.ammo = 0
-        self.knife_cooldown = 0
-
-        for num in range(1, 5):
-            img_right = pygame.image.load(path + f"/res/images/man2_{num}.png")
-            img_right = pygame.transform.scale(img_right, (40, 75))
-            img_left = pygame.transform.flip(img_right, True, False)
-            self.images_right.append(img_right)
-            self.images_left.append(img_left)
-        for num in range(1, 5):
-            img_right_openchest = pygame.image.load(path + f"/res/images/gun_man2_{num}.png")
-            img_right_openchest = pygame.transform.scale(img_right_openchest, (40, 75))
-            img_left_openchest = pygame.transform.flip(img_right_openchest, True, False)
-            self.images_right_openchest.append(img_right_openchest)
-            self.images_left_openchest.append(img_left_openchest)
-        self.image = self.images_right[self.index]
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.width = self.image.get_width()
-        self.height = self.image.get_height()
-        self
-        self.vel_y = 0
-        self.jumped = False
-        self.direction = 0
-        self.health = 100
-
-    def update(self):
-        global shoot2, knife2
-        dx= 0
-        dy= 0
-        walk_cooldown = 7
-        if dead == False:
-            key = pygame.key.get_pressed()
-            if key[pygame.K_i] and not self.jumped and self.grounded:
-                self.jumped = True
-                self.grounded = False
-                self.vel_y =- 17
-            elif key[pygame.K_UP] and not self.jumped and self.grounded:
-                self.jumped = True
-                self.grounded = False
-                self.vel_y =- 17
-                
-            if key[pygame.K_i] or key[pygame.K_UP] == False:
-                self.jumped = False
-            if key[pygame.K_j] or key[pygame.K_LEFT]:
-                dx -= 7
-                self.counter += 1
-                self.direction = -1
-            if key[pygame.K_l]or key[pygame.K_RIGHT]:
-                dx += 7
-                self.counter += 1
-                self.direction = 1
-            if key[pygame.K_j] == False and key[pygame.K_l] == False and key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
-                self.counter = 0
-                self.index = 0
-            if (chest.is_open and chest.opened_by == self) or (chest2.is_open and chest2.opened_by == self):
-                if key[pygame.K_m]:
-                    shoot2 = True
-            if self.ammo == 0:
-                shoot2 = False
-            if self.rect.colliderect(player):
-                if key[pygame.K_COMMA]:
-                    knife2 = True
-            if not self.rect.colliderect(player):
-                knife2 = False
-
-        if (chest.is_open and chest.opened_by == self) or (chest2.is_open and chest2.opened_by == self):
-            if self.direction == -1:
-                self.image = self.images_right_openchest[self.index]
-            if self.direction == 1:
-                self.image = self.images_left_openchest[self.index]
-        else:
-            if self.direction == -1:
-                self.image = self.images_right[self.index]
-            if self.direction == 1:
-                self.image = self.images_left[self.index]
-            
-        
-        if self.counter > walk_cooldown:
-            self.counter = 0
-            self.index += 1
-            if self.index >= len(self.images_right):
-                self.index = 0
-            if self.direction == -1:
-                self.image = self.images_right[self.index]
-            if self.direction == 1:
-                self.image = self.images_left[self.index]
-        
-        self.vel_y += 1
-        if self.vel_y > 10:
-            self.vel_y = 10
-        dy += self.vel_y
-        
-        for tile in world.tile_list:
-            if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
-                dx = 0
-            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
-                if self.vel_y < 0:
-                    self.rect.top = tile[1].bottom
-                    self.vel_y = 0
-                    dy = 0
-                elif self.vel_y >= 0:    
-                    self.rect.bottom = tile[1].top
-                    dy = 0
-                    self.vel_y = 0
-                    self.grounded = True
-
-        self.rect.x += dx
-        self.rect.y += dy
-        
-        
-        if self.rect.bottom > screen_height:
-            self.rect.bottom = screen_height
-            dy = 0
-            self.grounded = True
-        
-        screen.blit(self.image, self.rect)
-        
-        if self.rect.colliderect(chest.rect):
-            key = pygame.key.get_pressed()
-            if key[pygame.K_n] and not chest.is_open:
-                chest.open(self)
-                self.ammo += 50
-        if self.rect.colliderect(chest2.rect):
-            key = pygame.key.get_pressed()
-            if key[pygame.K_n] and not chest2.is_open:
-                chest2.open(self)
-                self.ammo += 50
-                
-
-        if self.shoot2_cooldown > 0:
-            self.shoot2_cooldown -= 1
-        if shoot2:
-            if self.shoot2_cooldown == 0:
-                self.shoot2_cooldown = 8
-                bullet2 = Bullet2(player2.rect.centerx + (0.6 * player2.rect.size[0] * player2.direction), player2.rect.centery, player2.direction)
-                bullet_group2.add(bullet2)
-                self.ammo -= 1
-
-        if self.knife_cooldown > 0:
-            self.knife_cooldown -= 1
-        if knife2:
-            if self.knife_cooldown == 0:
-                self.knife_cooldown = 20
-                player.health -= 10
 
 class Player():
-    def __init__(self, x, y):
+    def __init__(self, x, y, player_type, keybinds):
         self.images_right = []
         self.images_left = []
         self.images_right_openchest = []
@@ -201,15 +44,16 @@ class Player():
         self.shoot_cooldown = 0
         self.ammo = 0
         self.knife_cooldown = 0
+        self.player_type = player_type
 
         for num in range(1, 5):
-            img_right = pygame.image.load(path + f"/res/images/man_{num}.png")
+            img_right = pygame.image.load(path + f"/res/images/man{player_type}_{num}.png")
             img_right = pygame.transform.scale(img_right, (40, 75))
             img_left = pygame.transform.flip(img_right, True, False)
             self.images_right.append(img_right)
             self.images_left.append(img_left)
         for num in range(1, 5):
-            img_right_openchest = pygame.image.load(path + f"/res/images/gun_man_{num}.png")
+            img_right_openchest = pygame.image.load(path + f"/res/images/gun_man{player_type}_{num}.png")
             img_right_openchest = pygame.transform.scale(img_right_openchest, (40, 75))
             img_left_openchest = pygame.transform.flip(img_right_openchest, True, False)
             self.images_right_openchest.append(img_right_openchest)
@@ -225,40 +69,56 @@ class Player():
         self.direction = 0
         self.health = 100
         
+        # Keybinds
+        self.jump = keybinds['jump']
+        self.left = keybinds['left']
+        self.right = keybinds['right']
+        self.shoot = keybinds['shoot']
+        self.knife = keybinds['knife']
+        self.loot = keybinds['loot']
+        
     def update(self):
         global shoot, knife
+        if self.player_type == 1:
+            self.player = player
+            self.opposite_player = player2
+            self.shot_id = shoot
+        else:
+            self.player = player2
+            self.opposite_player = player
+            self.shot_id = shoot2
         dx= 0
         dy= 0
         walk_cooldown = 7
         if dead == False:
             #KeyStrokes
             key = pygame.key.get_pressed()
-            if key[pygame.K_w] and not self.jumped and self.grounded:
+            if key[self.jump] and not self.jumped and self.grounded:
                 self.jumped = True
                 self.grounded = False
                 self.vel_y =- 17
-            if key[pygame.K_w] == False:
+            if key[self.jump] == False:
                 self.jumped = False
-            if key[pygame.K_a]:
+            if key[self.left]:
                 dx -= 7       
                 self.counter += 1
                 self.direction = -1
-            if key[pygame.K_d]:
+            if key[self.right]:
                 dx += 7
                 self.counter += 1
                 self.direction = 1
-            if key[pygame.K_a] == False and key[pygame.K_d] == False:
+            if key[self.left] == False and key[self.right] == False:
                 self.counter = 1
                 self.index = 1
             if (chest.is_open and chest.opened_by == self) or (chest2.is_open and chest2.opened_by == self):
-                if key[pygame.K_SPACE]:
-                    shoot = True
+                if key[self.shoot]:
+                    self.shot_id = True
             if self.ammo == 0:
-                shoot = False
-            if self.rect.colliderect(player2):
-                if key[pygame.K_c]:
+                self.shot_id = False
+            if self.rect.colliderect(self.opposite_player):
+                if key[self.knife]:
                     knife = True
-            if not self.rect.colliderect(player2):
+            if not self.rect.colliderect(self.opposite_player):
                 knife = False
 
         if (chest.is_open and chest.opened_by == self) or (chest2.is_open and chest2.opened_by == self):
@@ -316,21 +176,24 @@ class Player():
         
         if self.rect.colliderect(chest.rect):
             key = pygame.key.get_pressed()
-            if key[pygame.K_b] and not chest.is_open:
+            if key[self.loot] and not chest.is_open:
                 chest.open(self)
                 self.ammo += 50
         if self.rect.colliderect(chest2.rect):
             key = pygame.key.get_pressed()
-            if key[pygame.K_b] and not chest2.is_open:
+            if key[self.loot] and not chest2.is_open:
                 chest2.open(self)
                 self.ammo += 50
                 
         if self.shoot_cooldown > 0:
             self.shoot_cooldown -= 1
-        if shoot:
+        if self.shot_id:
             if self.shoot_cooldown == 0:
                 self.shoot_cooldown = 8
-                bullet = Bullet(player.rect.centerx + (0.6 * player.rect.size[0] * player.direction), player.rect.centery, player.direction)
+                if self.player_type == 1:
+                    bullet = Bullet(self.player.rect.centerx + (0.6 * self.player.rect.size[0] * self.player.direction), self.player.rect.centery, self.player.direction)
+                else:
+                    bullet = Bullet2(self.player.rect.centerx + (0.6 * self.player.rect.size[0] * self.player.direction), self.player.rect.centery, self.player.direction)
                 bullet_group.add(bullet)
                 self.ammo -= 1
 
@@ -339,7 +202,7 @@ class Player():
         if knife:
             if self.knife_cooldown == 0:
                 self.knife_cooldown = 20
-                player2.health -= 10
+                self.opposite_player.health -= 10
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self,  x, y, direction):
@@ -493,9 +356,23 @@ def initialize_game():
     world = World(world_data)
     chest = Chest(965, 598, chest_closed_img, chest_open_img)
     chest2 = Chest(185, 598, chest_closed_img, chest_open_img)
-    player2 = Player2(650, 25)
-    player = Player(530, 25)
-
+    player2 = Player(650, 25, 2, {
+        "shoot": pygame.K_m,
+        "right": pygame.K_RIGHT,
+        "left": pygame.K_LEFT,
+        "jump": pygame.K_UP,
+        "loot": pygame.K_DOWN,
+        "knife": pygame.K_COMMA
+    })
+    player = Player(530, 25, 1, {
+        "shoot": pygame.K_SPACE,
+        "right": pygame.K_d,
+        "left": pygame.K_a,
+        "jump": pygame.K_w,
+        "loot": pygame.K_s,
+        "knife": pygame.K_c
+    })
+    
 def draw_health():
     font = pygame.font.Font(None, 36)
     text = font.render(f'Health: {player.health}', True, (255,0, 68))
@@ -572,8 +449,22 @@ world_data = [
 [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
 ]
 
-player2 = Player2(650, 25)
-player = Player(530, 25)
+player2 = Player(650, 25, 2, {
+    "shoot": pygame.K_m,
+    "right": pygame.K_RIGHT,
+    "left": pygame.K_LEFT,
+    "jump": pygame.K_UP,
+    "loot": pygame.K_DOWN,
+    "knife": pygame.K_COMMA
+})
+player = Player(530, 25, 1, {
+    "shoot": pygame.K_SPACE,
+    "right": pygame.K_d,
+    "left": pygame.K_a,
+    "jump": pygame.K_w,
+    "loot": pygame.K_s,
+    "knife": pygame.K_c
+})
 world = World(world_data)
 
 run = True
